5e69d7b5022f40bb39297d397fe2310892365f11
christopherkim8 <57571449+christopherkim8@users.noreply.github.com>
2025-04-12 01:35:23 -0700

    Contact Me Functionality
    Made each form in contact me required and using formspree to get forms sent to email.
    Also changed javascript file to use jquery

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const menuBtn = document.querySelector('.menu-toggle');
-const navbar = document.querySelector('.navbar');
-
-menuBtn.addEventListener('click', () => {
-  navbar.classList.toggle('active');
+$(document).ready(function () {
+  // Toggle navbar menu
+  $('.menu-toggle').on('click', function () {
+    $('.navbar').toggleClass('active');
+  });
 });
